13123654b5bb9e05832ee9175ef5e21068528320
jiaosir123 <43516980+jiaosir123@users.noreply.github.com>
2018-09-27 19:37:59 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
 module.exports = function main() {
+    var sum=0;
+var n=paseInt(arguments[0]);
+while(n!==0)
+{
+sum=sum+n%10;
+var n=Math.floor(n/10);
+}
+return sum;
   // Write your code here
 };
